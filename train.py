e896798c5cb306809cc78981f8b7935dc3cf7fe3
yassinek02 <119341910+yassinek02@users.noreply.github.com>
2023-11-13 00:52:38 +0100

    Update train.py

--- train.py
+++ train.py
@@ -7,22 +7,31 @@ import torch.nn as nn
 import torch.optim as optim
 
 
-from model import Generator, Discriminator
+from model import Generator, Discriminator, WeightNetwork, train_weight_network
 from utils import D_train, G_train, save_models
-
-
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train Normalizing Flow.')
-    parser.add_argument("--epochs", type=int, default=100,
+    parser.add_argument("--epochs", type=int, default=400,
                         help="Number of epochs for training.")
-    parser.add_argument("--lr", type=float, default=0.002,
+    parser.add_argument("--lr", type=float, default=0.0002,
                       help="The learning rate to use for training.")
-    parser.add_argument("--batch_size", type=int, default=64, 
+    parser.add_argument("--batch_size", type=int, default=32,
+                        help="Size of mini-batches for SGD")
+    parser.add_argument("--mnist_size", type=int, default=10000,
                         help="Size of mini-batches for SGD")
 
+
     args = parser.parse_args()
+
+        # Check if GPU is available
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+        print('GPU is available')
+    else:
+        device = torch.device("cpu")
+        print('GPU is not available')
 
 
     os.makedirs('chekpoints', exist_ok=True)
@@ -39,44 +48,63 @@ if __name__ == '__main__':
     test_dataset = datasets.MNIST(root='data/MNIST/', train=False, transform=transform, download=False)
 
 
-    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
-                                               batch_size=args.batch_size, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
+    mnist_size = args.mnist_size
+    if mnist_size != 0:
+        mnist_trainset_reduced = torch.utils.data.random_split(train_dataset, [mnist_size, len(train_dataset)-mnist_size])[0] 
+        train_loader = torch.utils.data.DataLoader(mnist_trainset_reduced, batch_size=args.batch_size, shuffle=True)
+        # download test dataset
+        max_mnist_size = mnist_size // 2
+        mnist_testset_reduced = torch.utils.data.random_split(test_dataset, [max_mnist_size, len(test_dataset)-max_mnist_size])[0] 
+        test_loader = torch.utils.data.DataLoader(mnist_testset_reduced, batch_size=args.batch_size, shuffle=True)
+    else:
+        train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
+                                                   batch_size=args.batch_size, shuffle=True)
+        test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
                                               batch_size=args.batch_size, shuffle=False)
     print('Dataset Loaded.')
 
 
     print('Model Loading...')
     mnist_dim = 784
-    G = torch.nn.DataParallel(Generator(g_output_dim = mnist_dim)).cuda()
-    D = torch.nn.DataParallel(Discriminator(mnist_dim)).cuda()
+    G = torch.nn.DataParallel(Generator(g_output_dim = mnist_dim)).to(device)
+    D = torch.nn.DataParallel(Discriminator(mnist_dim)).to(device)
 
 
-    # model = DataParallel(model).cuda()
+    # model = DataParallel(model).to(device)
     print('Model loaded.')
     # Optimizer 
 
 
 
     # define loss
-    criterion = nn.BCELoss() 
+    criterion = nn.BCELoss()
+
+    # Initialisation du réseau de poids d'importance et de son optimiseur
+    w_net = WeightNetwork().to(device)
+    w_optimizer = optim.Adam(w_net.parameters(), lr=args.lr)
+
+    # Hyperparamètres pour l'entraînement de w_ϕ
+    lambda1, lambda2, m, nd= 10, 10, 1, 1  # Ces valeurs doivent être ajustées selon vos besoins
 
     # define optimizers
-    G_optimizer = optim.Adam(G.parameters(), lr = args.lr)
-    D_optimizer = optim.Adam(D.parameters(), lr = args.lr)
+    G_optimizer = optim.Adam(G.parameters(), lr = args.lr, betas=(0.5, 0.999))
+    D_optimizer = optim.Adam(D.parameters(), lr = args.lr, betas=(0.5, 0.999))
 
     print('Start Training :')
     
     n_epoch = args.epochs
     for epoch in trange(1, n_epoch+1, leave=True):           
         for batch_idx, (x, _) in enumerate(train_loader):
-            x = x.view(-1, mnist_dim)
+            x = x.view(-1, mnist_dim).to('cpu')
             D_train(x, G, D, D_optimizer, criterion)
             G_train(x, G, D, G_optimizer, criterion)
+
+            # Entraînement du réseau de poids d'importance w_ϕ
+            # Cette fonction doit être définie pour intégrer l'algorithme de formation de w_ϕ
+            train_weight_network(x, G, D, w_net, w_optimizer, D_optimizer, criterion, lambda1, lambda2, m)
 
         if epoch % 10 == 0:
             save_models(G, D, 'checkpoints')
                 
     print('Training done')
-
-        
+    
